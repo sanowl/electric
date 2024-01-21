46a4aa23783461db6278890a0245050638cfd500
san.owl <99511815+sanowl@users.noreply.github.com>
2024-01-21 12:56:32 +0300

    under

--- generator/src/generator.ts
+++ generator/src/generator.ts
@@ -1,57 +1,60 @@
-import { GeneratorOptions } from '@prisma/generator-helper'
-import { z } from 'zod'
+import { GeneratorOptions } from '@prisma/generator-helper';
+import { z } from 'zod';
 
-import { DirectoryHelper, ExtendedDMMF } from './classes'
-import { generateMultipleFiles } from './generateMultipleFiles'
-import { generateSingleFile } from './generateSingleFile'
-import { skipGenerator } from './utils'
+import { DirectoryHelper, ExtendedDMMF } from './classes';
+import { generateMultipleFiles } from './generateMultipleFiles';
+import { generateSingleFile } from './generateSingleFile';
+import { skipGenerator } from './utils';
 
 export interface GeneratorConfig {
-  output: GeneratorOptions['generator']['output']
-  config: GeneratorOptions['generator']['config']
-  dmmf: GeneratorOptions['dmmf']
-  datamodel: string
+  output: GeneratorOptions['generator']['output'];
+  config: GeneratorOptions['generator']['config'];
+  dmmf: GeneratorOptions['dmmf'];
+  datamodel: string;
 }
 
+// Define zod schema for output configuration
 const outputSchema = z.object({
   fromEnvVar: z.string().nullable(),
   value: z.string({ required_error: 'No output path specified' }),
-})
+});
 
 export const generator = async (config: GeneratorConfig) => {
-  const output = outputSchema.parse(config.output)
+  // Parse and validate output configuration using zod schema
+  const output = outputSchema.parse(config.output);
 
+  // Check if generator should be skipped
   if (await skipGenerator()) {
     return console.log(
       '\x1b[33m',
       '!!!! Generation of zod schemas skipped! Generator is disabled in "zodGenConfig.js" !!!!',
       '\x1b[37m'
-    )
+    );
   }
 
-  // extend the DMMF with custom functionality - see "classes" folder
+  // Extend the DMMF with custom functionality - see "classes" folder
   const extendedDMMF = new ExtendedDMMF(
     config.dmmf,
     config.config,
     config.datamodel
-  )
+  );
 
   // If data is present in the output directory, delete it.
-  DirectoryHelper.removeDir(output.value)
+  DirectoryHelper.removeDir(output.value);
 
   // Create the output directory
-  DirectoryHelper.createDir(output.value)
+  DirectoryHelper.createDir(output.value);
 
-  // generate single or multiple files
+  // Generate single or multiple files based on configuration
   if (extendedDMMF.generatorConfig.useMultipleFiles) {
     return generateMultipleFiles({
       dmmf: extendedDMMF,
       path: output.value,
-    })
+    });
   }
 
   return generateSingleFile({
     dmmf: extendedDMMF,
     path: output.value,
-  })
-}
+  });
+};
